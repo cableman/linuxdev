79b0697c45448c9e736f8f416883fd36338a14af
Jesper Kristensen <cableman@linuxdev.dk>
2011-08-17 21:57:13 +0200

    Added imdb callback

---
+++ sites/all/modules/imdb/js/imdb.js
@@ -0,0 +1,12 @@
+(function ($) {
+  $(document).ready(function() {
+    button = $('<button id="imdb-button" type="button">Use IMDB</button>')
+    $('#edit-field-imdb-id-und-0-value').parent().append(button);
+    button.show();
+
+    // Add click action
+    button.click(function () {
+      
+    });
+  });
+})(jQuery);
